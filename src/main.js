0417634c1bf26403cb5b075ea49e94ec086454d8
new_user_m.fouad <mohamed.fouad@3ddx.com>
2019-05-18 18:19:42 +0200

    git init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
+import './assets/css/style.css'
 
 Vue.config.productionTip = false
 
